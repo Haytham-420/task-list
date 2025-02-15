4053d46409fe4ef5c7c52df0139b9e72e8bed645
Haytham-420 <haytham2042002@gmail.com>
2025-02-15 20:35:12 +0200

    homework1

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,36 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    $name = 'Haytham';
+
+    //first method
+    //return view('about',['name'=> $name] );
+
+    //second method
+    // return view('about')->with('name', $name);
+
+    $departments = [
+        '01' => 'Technical',
+        '02' => 'Financial',
+        '03' => 'Sales',
+    ];
+
+    //third method
+    return view('about', compact('name', 'departments'));
+});
+
+// we can use the same route twice IF we use a different method each
+//time (eg. get, post), or it will be overwritten by the latest in the php file
+
+Route::post('/about', function () {
+    $name = $_POST['name'];
+    $departments = [
+        '01' => 'Technical',
+        '02' => 'Financial',
+        '03' => 'Sales',
+    ];
+
+    return view('about', compact('name', 'departments'));
+});
